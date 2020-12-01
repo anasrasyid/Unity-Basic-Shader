860f18fcae229317bd8c358fe4d9d7e09777352a
Anas Rasyid <anasrasyid333@gmail.com>
2020-12-01 18:38:55 +0700

    Basic Graph
    F(x) = x*x

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefabs = default;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    private void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        var scale = Vector3.one * step;
+        for (int i = 0; i < resolution; i++)
+        {
+            Transform point = Instantiate(pointPrefabs);
+
+            position.x = ((i + 0.5f) * step - 1f);
+            position.y = position.x * position.x;
+
+            point.position = position;
+            point.localScale = scale;
+
+            point.SetParent(transform, false);
+        }
+    }
+}
