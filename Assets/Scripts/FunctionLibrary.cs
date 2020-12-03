980cdbfbb2cb637cd93a6afeaaa1b80680924b8d
Anas Rasyid <anasrasyid333@gmail.com>
2020-12-03 15:53:05 +0700

    simplify function and 3D graph

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -4,7 +4,7 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary
 {
     public delegate float Function(float x, float z);
-    public delegate float Function3D(float x, float z, float t);
+    public delegate Vector3 Function3D(float u, float v, float t);
 
     public enum FunctionName { Wave, MultiWave, Ripple }
 
@@ -40,23 +40,31 @@ public static class FunctionLibrary
         return y / (1f + 10f * d);
     }
 
-    public static float Wave(float x, float z, float t)
+    public static Vector3 Wave(float u, float v, float t)
     {
-        return Sin(PI * (x + z + t));
+        Vector3 p = new Vector3(u, 0, v);
+        p.y = Sin(PI * (u + v + t));
+        return p;
     }
 
-    public static float MultiWave(float x, float z, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Sin(PI * (x + 0.5f * t));
-        y += 0.5f * Sin(2f * PI * (z + t));
-        y += Sin(PI * (x + z + 0.25f * t));
-        return y * (1f / 2.5f);
+        Vector3 p = new Vector3(u, 0, v);
+
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= 1f / 2.5f;
+
+        return p;
     }
 
-    public static float Ripple(float x, float z, float t)
+    public static Vector3 Ripple(float u, float v, float t)
     {
-        float d = Sqrt(x * x + z * z);
-        float y = Sin(PI *(4f * d - t));
-        return y / (1f + 10f * d);
+        float d = Sqrt(u * u + v * v);
+        Vector3 p = new Vector3(u, 0, v);
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= 1f + 10f * d;
+        return p;
     }
 }
